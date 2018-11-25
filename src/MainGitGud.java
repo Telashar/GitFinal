3cf4c34469cbd963a6f3f5d814aa981f17afa463
Telashar <kaedeslawk@gmail.com>
2018-11-25 13:36:06 +0100

    Update

--- src/MainGitGud.java
+++ src/MainGitGud.java
@@ -1,8 +1,8 @@
 public class MainGitGud {
     public static void main(String[] args) {
         System.out.println("Hello GitGud World!");
-        for (int i = 0; i <3 ; i++) {
-            System.out.println("GitGud");
+        for (int j = 10; j >= 0; j -=2) {
+            System.out.println(j);
 
         }
 
