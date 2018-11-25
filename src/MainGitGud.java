abfaea10bc7e0d7c4d18e586914ba48511f61646
Telashar <kaedeslawk@gmail.com>
2018-11-25 12:35:21 +0100

    Updated GitGud

--- src/MainGitGud.java
+++ src/MainGitGud.java
@@ -1,6 +1,10 @@
 public class MainGitGud {
     public static void main(String[] args) {
         System.out.println("Hello GitGud World!");
-        
+        for (int i = 0; i <3 ; i++) {
+            System.out.println("GitGud");
+
+        }
+
     }
 }
