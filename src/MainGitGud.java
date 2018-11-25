21734f65b5f4d349fc65bdc1e46c603017439b6b
Telashar <kaedeslawk@gmail.com>
2018-11-25 15:34:04 +0100

    Update!

--- src/MainGitGud.java
+++ src/MainGitGud.java
@@ -3,6 +3,7 @@
 public class MainGitGud {
     public static void main(String[] args) {
         System.out.println("INCREDEEEBILISSS");
+        System.out.println("Jem kredki chlebem ze swoim starym");
 
     }
 }
